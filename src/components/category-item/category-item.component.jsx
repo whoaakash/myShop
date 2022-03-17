6dd48e4a9b84db3debd35326dff7044b2bae52c6
whoaakash <akast2041@gmail.com>
2022-03-17 17:52:43 +0530

    first commit

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import './category-item.component.scss';
+
+
+
+const CategoryItem = ({ category }) =>{
+    console.log(category)
+     const {imageUrl , title} = category;
+    // debugger
+     return(
+        <div className="category-container"> 
+        <div className="background-image" style={{
+          backgroundImage: `url(${imageUrl})` 
+        }} /> 
+        {/* <img/> */}
+        <div className="category-body-container">
+          <h2>{title}</h2>
+          <p>SHOP NOW</p>
+        </div>
+      </div>
+     )
+} 
+export default CategoryItem;
